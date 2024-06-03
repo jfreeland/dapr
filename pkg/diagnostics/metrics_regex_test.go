358d150b5b3871c88212df35dbb0fd0127621888
Joey Freeland <joey@free.land>
2024-06-03 18:08:46 -0400

    chore: get tests to pass
    Signed-off-by: Joey Freeland <joey@free.land>

--- pkg/diagnostics/metrics_regex_test.go
+++ pkg/diagnostics/metrics_regex_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
@@ -13,6 +14,62 @@ import (
 	diagUtils "github.com/dapr/dapr/pkg/diagnostics/utils"
 )
 
+const (
+	componentLatencyConfigurationName     = "component/configuration/latencies"
+	componentLatencyCryptoName            = "component/crypto/latencies"
+	componentLatencyInputBindingName      = "component/input_binding/latencies"
+	componentLatencyOutputBindingName     = "component/output_binding/latencies"
+	componentLatencyPubsubEgressBulkName  = "component/pubsub_egress/bulk/latencies"
+	componentLatencyPubsubEgressName      = "component/pubsub_egress/latencies"
+	componentLatencyPubsubIngressBulkName = "component/pubsub_ingress/bulk/latencies"
+	componentLatencyPubsubIngressName     = "component/pubsub_ingress/latencies"
+	componentLatencySecretName            = "component/secret/latencies"
+	componentLatencyStateName             = "component/state/latencies"
+	grpcHealthprobesLatencyName           = "grpc.io/healthprobes/roundtrip_latency"
+	grpcRoundtripLatencyName              = "grpc.io/client/roundtrip_latency"
+	grpcServerLatencyName                 = "grpc.io/server/server_latency"
+	httpClientRoundtripLatencyName        = "http/client/roundtrip_latency"
+	httpHealthprobesLatencyName           = "http/healthprobes/roundtrip_latency"
+	httpServerLatencyName                 = "http/server/latency"
+	resiliencyActivationViewName          = "resiliency/activations_total"
+	resiliencyCountViewName               = "resiliency/count"
+	resiliencyLoadedViewName              = "resiliency/loaded"
+	serviceInvocationRecvLatencyName      = "runtime/service_invocation/res_recv_latency_ms"
+	workflowActivityLatencyName           = "runtime/workflow/activity/execution/latency"
+	workflowExecutionLatencyName          = "runtime/workflow/execution/latency"
+	workflowOperationLatencyName          = "runtime/workflow/operation/latency"
+	workflowSchedulingLatencyName         = "runtime/workflow/scheduling/latency"
+)
+
+func cleanupRegisteredViews() {
+	CleanupRegisteredViews(
+		componentLatencyConfigurationName,
+		componentLatencyCryptoName,
+		componentLatencyInputBindingName,
+		componentLatencyOutputBindingName,
+		componentLatencyPubsubEgressBulkName,
+		componentLatencyPubsubEgressName,
+		componentLatencyPubsubIngressBulkName,
+		componentLatencyPubsubIngressName,
+		componentLatencySecretName,
+		componentLatencyStateName,
+		grpcHealthprobesLatencyName,
+		grpcRoundtripLatencyName,
+		grpcServerLatencyName,
+		httpClientRoundtripLatencyName,
+		httpHealthprobesLatencyName,
+		httpServerLatencyName,
+		resiliencyActivationViewName,
+		resiliencyCountViewName,
+		resiliencyLoadedViewName,
+		serviceInvocationRecvLatencyName,
+		workflowActivityLatencyName,
+		workflowExecutionLatencyName,
+		workflowOperationLatencyName,
+		workflowSchedulingLatencyName,
+	)
+}
+
 func TestRegexRulesSingle(t *testing.T) {
 	const statName = "test_stat_regex"
 	methodKey := tag.MustNewKey("method")
@@ -20,7 +77,9 @@ func TestRegexRulesSingle(t *testing.T) {
 	latencyDistributionBuckets := []float64{5, 50, 500, 5_000}
 	latencyDistribution := view.Distribution(latencyDistributionBuckets...)
 
-	InitMetrics("testAppId2", "", []config.MetricsRule{
+	cleanupRegisteredViews()
+
+	err := InitMetrics("testAppId2", "", []config.MetricsRule{
 		{
 			Name: statName,
 			Labels: []config.MetricLabel{
@@ -34,6 +93,9 @@ func TestRegexRulesSingle(t *testing.T) {
 			},
 		},
 	}, false, latencyDistributionBuckets)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	t.Run("single regex rule applied", func(t *testing.T) {
 		view.Register(
@@ -43,11 +105,13 @@ func TestRegexRulesSingle(t *testing.T) {
 			view.Unregister(view.Find(statName))
 		})
 
+		spew.Dump(diagUtils.WithTags(testStat.Name(), methodKey, "/orders/456"))
 		stats.RecordWithTags(context.Background(),
 			diagUtils.WithTags(testStat.Name(), methodKey, "/orders/123"),
 			testStat.M(1))
 
 		viewData, _ := view.RetrieveData(statName)
+		spew.Dump(viewData)
 		v := view.Find(statName)
 
 		allTagsPresent(t, v, viewData[0].Tags)
